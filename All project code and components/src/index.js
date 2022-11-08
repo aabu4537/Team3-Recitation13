668f2c99a5ac6174691a8210efd24558bcb2ddb2
dmboynton56 <dmboynton56@gmail.com>
2022-11-08 10:17:21 -0700

    initial commit - partials etc.

---
+++ All project code and components/src/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const pgp = require("pg-promise")();
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+// db config
+const dbConfig = {
+  host: "db",
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
+
+const db = pgp(dbConfig);
+
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      saveUninitialized: false,
+      resave: false,
+    })
+  );
+  
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+  
+  app.get('/', (req, res) =>{
+    res.redirect('/login'); 
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("pages/login");
+  });
