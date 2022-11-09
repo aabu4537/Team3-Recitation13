9a333a58fe4772c17788191ab62dd3cae19a62ad
EricGraham7 <106702600+EricGraham7@users.noreply.github.com>
2022-11-09 15:05:57 -0700

    Fix engine and session bugs on index.js
    Also added the database connection test because why not

--- All project code and components/src/index.js
+++ All project code and components/src/index.js
@@ -15,6 +15,31 @@ const dbConfig = {
 
 const db = pgp(dbConfig);
 
+// db test
+db.connect()
+  .then((obj) => {
+    // Can check the server version here (pg-promise v10.1.0+):
+    console.log("Database connection successful");
+    obj.done(); // success, release the connection;
+  })
+  .catch((error) => {
+    console.log("ERROR:", error.message || error);
+  });
+  
+// set the view engine to ejs
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+
+// set session
+app.use(
+  session({
+    secret: "XASDASDA",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+/* This was the other version used on the labs; I am not sure about the difference, other than this might be more secure.
+
 app.use(
     session({
       secret: process.env.SESSION_SECRET,
@@ -22,7 +47,13 @@ app.use(
       resave: false,
     })
   );
-  
+
+ - <-> IMPORTANT!!! <> -
+If you want to use this method instead, you MUST add the following line of code to the .env file:
+SESSION_SECRET="super duper secret!"
+
+*/
+
   app.use(
     bodyParser.urlencoded({
       extended: true,
