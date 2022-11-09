050152d734b636bcff481011d2f9e721ae44b120
EricGraham7 <106702600+EricGraham7@users.noreply.github.com>
2022-11-09 10:48:58 -0700

    Fixed foreign key bug and bad comments

--- All project code and components/src/init_data/create.sql
+++ All project code and components/src/init_data/create.sql
@@ -6,8 +6,8 @@ CREATE TABLE recipes (
   recipe_image VARCHAR(1000), 
   instructions VARCHAR(10000), /* is formatting multi-paragraph instructions going to be a problem?*/
   author_id INTEGER NOT NULL REFERENCES users(user_id),
-  cuisine_type VARCHAR(50),/* do I need a table for this?*/
-  rating REAL /* maybe too extra*/
+  cuisine_type VARCHAR(50),
+  rating REAL /* maybe too extra, feel free to remove*/
 );
 
 CREATE TABLE users (
@@ -20,10 +20,10 @@ CREATE TABLE users (
 CREATE TABLE IF NOT EXISTS ingredients(
   ingredient_id SERIAL PRIMARY KEY NOT NULL,  
   ingredient_name varchar(50) NOT NULL,
-  dietary_warning BOOLEAN /* too extra? // put this here or in a different table?*/
+  dietary_warning BOOLEAN /* maybe too extra, feel free to remove*/
   );
 
-CREATE TABLE IF NOT EXISTS favorites( /* since this is a many-to-many relationship between users and recipes (unlike the author_id in recipes table) we need a bridge table, it will also e nice to keep it seperate*/
+CREATE TABLE IF NOT EXISTS favorites( /* since this is a many-to-many relationship between users and recipes (unlike the author_id in recipes table) we need a bridge table */
   user_id INTEGER NOT NULL REFERENCES users(user_id),
   recipe_id INTEGER NOT NULL REFERENCES recipes(recipe_id),
   PRIMARY KEY(user_id, recipe_id) /* this defines a composite primary key consisting of two halves: the user_id foreign key and the recipe_id foreign key*/
@@ -32,5 +32,5 @@ CREATE TABLE IF NOT EXISTS favorites( /* since this is a many-to-many relationsh
 CREATE TABLE IF NOT EXISTS recipes_to_ingredients(
   recipe_id INTEGER NOT NULL REFERENCES recipes(recipe_id),
   ingredient_id INTEGER NOT NULL REFERENCES ingredients(ingredient_id),
-  PRIMARY KEY(recipe_id,) /* this defines a composite primary key consisting of two halves: the user_id foreign key and the recipe_id foreign key*/
+  PRIMARY KEY(recipe_id,ingredient_id) /* this defines a composite primary key consisting of two halves: the recipe_id foreign key and the ingredient_id foreign key*/
 );
