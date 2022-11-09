12c105bab0dd6f458b0158447f52c9f4af58879f
EricGraham7 <106702600+EricGraham7@users.noreply.github.com>
2022-11-09 16:22:47 -0700

    Added POST login route + comments
    Also commented on the need to change the DB in the .env file when database is done, and added many comments to index.js
    Note that the route isn't 100% functional yet because it depends on the database and html pages - which aren't sending password information yet

--- All project code and components/src/index.js
+++ All project code and components/src/index.js
@@ -3,6 +3,8 @@ const app = express();
 const pgp = require("pg-promise")();
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const bcrypt = require('bcrypt');
+const { queryResult } = require("pg-promise");
 
 // db config
 const dbConfig = {
@@ -60,19 +62,69 @@ SESSION_SECRET="super duper secret!"
     })
   );
   
-  app.get('/', (req, res) =>{
+//We will use this to store the user's information in a session variable.
+const user = {
+  user_id: undefined,
+  username: undefined,
+  email: undefined,
+};
+
+
+  //Redirect from / to /login - once we have a home page, can change to home
+app.get('/', (req, res) =>{
     res.redirect('/login'); 
-  });
+});
 
-  app.get("/login", (req, res) => {
+//GET login
+app.get("/login", (req, res) => {
     res.render("pages/login");
+});
+
+
+//POST login - Ready to test when Database is done
+  // Note: Need to rectify the fact that we need a password yet the pages are only sending username and email
+  app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const query = 'SELECT * FROM users WHERE username = $1;';
+  
+    db.one(query, [username]) //query the database using the username provided at login
+      .then(async (queryResult) => {
+
+        //compare the hashed password in the database with the hashed version of the password the user provided
+        const match = await bcrypt.compare(req.body.password, queryResult.password);
+  
+        if(!match){   //if the hashed passwords didn't match, return error and redirect
+          console.log("Incorrect username or password");
+          res.redirect("/login");
+        }
+        else{   //If user was found, save the results of the query (except password) in a session variable
+
+          //updating the data of user
+          user.user_id = queryResult.user_id;
+          user.username = queryResult.username;
+          user.email = queryResult.email;
+
+          //make this user into a session variable
+          req.session.user = user;
+            req.session.save();
+            res.redirect("/profile");
+        }
+      })
+      //if error, return the error and render login with this error message so it can be displayed to user
+      .catch((err) => {
+        console.log(err);
+        res.render("pages/login", {
+          message: err,
+        });
+      });
   });
-
-
 
   // Authentication Middleware.
 const auth = (req, res, next) => {
+  //if no session variable, that means user is not logged in, so shouldn't be able to view profile, for example
   if (!req.session.user) {
+    //if the path is /register or /login, thats fine, do nothing. Otherwise, redirect to login
       if(req.path == "/register" || req.path == "/login"){
           return;
       }
