fcd89ad0175cacf86150846a68451a60f3132aeb
Kayla Zhong <kazh2627@colorado.edu>
2022-11-10 08:56:25 -0700

    Added another table and some insertions

--- All project code and components/src/init_data/create.sql
+++ All project code and components/src/init_data/create.sql
@@ -8,7 +8,6 @@ CREATE TABLE users (
 CREATE TABLE IF NOT EXISTS ingredients(
   ingredient_id SERIAL PRIMARY KEY NOT NULL,  
   ingredient_name varchar(50) NOT NULL,
-  dietary_warning BOOLEAN /* maybe too extra, feel free to remove*/
   );
 
 CREATE TABLE recipes (
@@ -19,10 +18,40 @@ CREATE TABLE recipes (
   recipe_image VARCHAR(1000), 
   instructions VARCHAR(10000), /* is formatting multi-paragraph instructions going to be a problem?*/
   author_id INTEGER NOT NULL REFERENCES users(user_id),
-  cuisine_type VARCHAR(50),
+  cuisine_type VARCHAR(50), /* might move this to filters */
   rating REAL /* maybe too extra, feel free to remove*/
 );
 
+CREATE TABLE filters (
+  filter_id SERIAL PRIMARY KEY NOT NULL, /* create separate tables or leave as is? */
+  vegan BOOLEAN, /* common diets */
+  vegetarian BOOLEAN,
+  keto BOOLEAN,
+  paleo BOOLEAN,
+  low_carb BOOLEAN,
+  high_carb BOOLEAN,
+  low_calorie BOOLEAN,
+  high_calorie BOOLEAN,
+  contains_dairy BOOLEAN, /* common allergens/restrictions/sensitivities */
+  contains_eggs BOOLEAN,
+  contains_nuts BOOLEAN,
+  contains_soy BOOLEAN,
+  contains_wheat BOOLEAN,
+  contains_meat BOOLEAN,
+  contains_pork BOOLEAN,
+  contains_fish BOOLEAN,
+  grain_free BOOLEAN,
+  gluten_free BOOLEAN,
+  under_30_minutes BOOLEAN, /* cook time */
+  30_minutes_1_hour BOOLEAN,
+  1_hour_2_hours BOOLEAN,
+  2_hours_3_hours BOOLEAN,
+  3_hours_or_more BOOLEAN,
+  this_week BOOLEAN, /* published */
+  this_month BOOLEAN,
+  this_year BOOLEAN,
+  today BOOLEAN
+);
 
 CREATE TABLE IF NOT EXISTS favorites( /* since this is a many-to-many relationship between users and recipes (unlike the author_id in recipes table) we need a bridge table */
   user_id INTEGER NOT NULL REFERENCES users(user_id),
--- All project code and components/src/init_data/insert.sql
+++ All project code and components/src/init_data/insert.sql
@@ -7,12 +7,28 @@ insert into users (username, email, password) values ('OldPerson', 'grandpa@gmai
 insert into users (username, email, password) values ('Light', 'deathnote@gmail.com', 'illtakeapotatochipandeatit');
 
 /* Initialize Ingredients Table*/
-insert into ingredients (ingredient_name, dietary_warning) values (, );
+insert into ingredients (ingredient_name) values (steak);
+insert into ingredients (ingredient_name) values (butter);
+insert into ingredients (ingredient_name) values (rosemary);
+insert into ingredients (ingredient_name) values (eggs);
+insert into ingredients (ingredient_name) values (canola oil);
+insert into ingredients (ingredient_name) values (olive oil);
 
 /* Initialize Recipes Table*/
-insert into recipes (recipe_name, prep_time, cook_time, recipe_image, instructions, author_id, cuisine_type, rating) values ('Steak and Eggs', 5, 20, 'https://healthyrecipesblogs.com/wp-content/uploads/2013/02/steak-and-eggs-featured-2021.jpg', 'Couldnt be bothered to add an actual instructions, so take a lorem ipsum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis commodo ullamcorper. Quisque tincidunt ac orci eget facilisis. Morbi porttitor eros nec imperdiet vehicula. Donec a massa et erat ullamcorper cursus. Nulla efficitur eu tellus sed venenatis. Aenean rutrum, dui in rutrum ullamcorper, ipsum orci fringilla tellus, vel luctus eros sapien quis eros. Donec eu enim condimentum, bibendum magna at, condimentum mauris. Nunc dictum ante quis nibh pellentesque, et tempor diam porta. In mollis ex sed massa euismod tincidunt. Nullam faucibus tristique massa eget egestas. Integer id ultricies eros. Pellentesque malesuada tempus congue.
-
-Phasellus at est orci. Donec iaculis ultricies urna sit amet dictum. Sed ultricies, lorem at auctor bibendum, enim sapien fringilla lorem, eget dictum massa nunc ac nisi. Etiam commodo non purus ac viverra. Duis aliquam felis id condimentum tristique. Donec interdum blandit auctor. Aliquam ac massa a lectus vulputate cursus. Proin consectetur ut tellus quis posuere. Pellentesque felis dolor, congue quis ipsum et, tempus scelerisque nulla. Maecenas nec interdum mauris, et pharetra nunc. Donec ut enim malesuada, volutpat erat eget, pulvinar urna. Aenean orci mauris, fringilla id dolor non, malesuada tincidunt nibh.', 1, 'American', 7.5);
+insert into recipes (recipe_name, prep_time, cook_time, recipe_image, instructions, author_id, cuisine_type, rating) 
+    values ('Steak and Eggs', 5, 20, 'https://healthyrecipesblogs.com/wp-content/uploads/2013/02/steak-and-eggs-featured-2021.jpg', 
+    'Couldnt be bothered to add an actual instructions, so take a lorem ipsum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Ut sagittis commodo ullamcorper. Quisque tincidunt ac orci eget facilisis. Morbi porttitor eros nec imperdiet vehicula. Donec a massa et 
+    erat ullamcorper cursus. Nulla efficitur eu tellus sed venenatis. Aenean rutrum, dui in rutrum ullamcorper, ipsum orci fringilla tellus, 
+    vel luctus eros sapien quis eros. Donec eu enim condimentum, bibendum magna at, condimentum mauris. Nunc dictum ante quis nibh pellentesque, 
+    et tempor diam porta. In mollis ex sed massa euismod tincidunt. Nullam faucibus tristique massa eget egestas. Integer id ultricies eros. 
+    Pellentesque malesuada tempus congue. Phasellus at est orci. Donec iaculis ultricies urna sit amet dictum. Sed ultricies, lorem at auctor 
+    bibendum, enim sapien fringilla lorem, eget dictum massa nunc ac nisi. Etiam commodo non purus ac viverra. Duis aliquam felis id condimentum 
+    tristique. Donec interdum blandit auctor. Aliquam ac massa a lectus vulputate cursus. Proin consectetur ut tellus quis posuere. 
+    Pellentesque felis dolor, congue quis ipsum et, tempus scelerisque nulla. Maecenas nec interdum mauris, et pharetra nunc. 
+    Donec ut enim malesuada, volutpat erat eget, pulvinar urna. Aenean orci mauris, fringilla id dolor non, malesuada tincidunt nibh.', 
+    1, 'American', 7.5);
+insert into recipes ()
 
 /* Initialize Favorites Table - FOR TESTING ONLY - should be added to only dynamically in production*/
 insert into favorites (user_id, recipe_id) values (, );
