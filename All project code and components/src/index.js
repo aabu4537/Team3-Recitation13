36527671a549d35b0fc0b60d1bacd86a23b9493e
EricGraham7 <106702600+EricGraham7@users.noreply.github.com>
2022-11-13 10:36:20 -0700

    Added GET /home, POST /favorite, misc updates
    - As comments say, haven't quite tested GET home, POST favorite does not handle errors well (particularly when you're trying to add a recipe to favorites that's already there), and GET /filter is in progress; the first couple of filters are done.
    - Also added an edge case catcher to GET profile - when user has no favorites at all
    - Minor comment editing

--- All project code and components/src/index.js
+++ All project code and components/src/index.js
@@ -4,6 +4,7 @@ const pgp = require("pg-promise")();
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const bcrypt = require('bcrypt');
+const { query } = require('express');
 //const { queryResult } = require("pg-promise");
 // db config
 const dbConfig = {
@@ -108,8 +109,6 @@ app.post('/register', async (req, res) => { //Input: username and password as JS
     });
 });
 
-
-
 //POST login - Tested - Done
   app.post("/login", (req, res) => {
     const username = req.body.username;
@@ -167,7 +166,7 @@ const auth = (req, res, next) => {
 */
 
 //Remaining routes to implement:
-//POST upload - called from a form probably, needs info from form, does not return anything (updates database)
+/*POST upload - called from a form probably, needs info from form, does not return anything (updates database)
   //(like POST register, except bigger and to recipes table, not users)
   
   //Need from HTML:
@@ -184,6 +183,7 @@ const auth = (req, res, next) => {
 
   //Return to HTML:
   //I will not return anything for the website to use, instead I will redirect to home.
+*/
 
 /*GET logout - called when clicked on logout button from other pages, renders login page with message, EXACTLY like labs
   Need: nothing
@@ -216,11 +216,21 @@ app.get("/logout", (req, res) => {
     //DONE - TESTED
 app.get("/profile", (req, res) => {
   query = 'SELECT recipes.recipe_id, recipes.recipe_name, recipes.prep_time, recipes.cook_time, recipes.recipe_image, recipes.recipe_ingredients, recipes.instructions, recipes.author_id, recipes.cuisine_type, recipes.rating, recipes.date_published, recipes.vegan, recipes.vegetarian, recipes.keto, recipes.paleo, recipes.grain_free, recipes.gluten_free, recipes.contains_dairy, recipes.contains_eggs, recipes.contains_nuts, recipes.contains_soy, recipes.contains_wheat, recipes.contains_beef, recipes.contains_pork, recipes.contains_fish, recipes.under_30_minutes, recipes.m30_minutes_1_hour, recipes.h1_hour_2_hours, recipes.h2_hours_3_hours, recipes.h3_hours_or_more, recipes.s1_star, recipes.s2_stars, recipes.s3_stars, recipes.s4_stars, recipes.s5_stars FROM users FULL JOIN favorites ON users.user_id = favorites.user_id FULL JOIN recipes ON favorites.recipe_id = recipes.recipe_id WHERE users.user_id = $1;'; //need to do a join on recipes, users, over favorites, so that we can select all recipes that are the favorite of the current user.
-  db.any(query, [req.session.user.user_id]) //note: MUST be db.any to return multipe query rows!
+  db.any(query, [req.session.user.user_id]) //note: MUST be db.any to return multipe query rows /recipes!
     .then(queryResult => {
+      
+      //little edge case: if the user hasn't favorited any recipes, then send EMPTY results, and error message
+      if(queryResult[0].recipe_id == null){
+        res.render("pages/profile", {
+          results: [],
+          message: 'No Favorited Recipes',
+        });
+        return;
+      }
+
       // show the object on console for reference, then render it to the page. On page, reference like results[0].recipe_name 
       console.log(queryResult);
-
+      
       res.render("pages/profile", {
       results: queryResult, //you will access in the EJS/HTML by calling results, not queryResult
     });
@@ -235,23 +245,14 @@ app.get("/profile", (req, res) => {
     });
 }); 
 
-//GET home - will render the HTML page home.ejs with a results object containing recipes. If called from its own page, this means it needs to filter the database before returning this. If called from somewhere else (i.e. menu bar), renders with all recipes.
+/*GET home - will render the HTML page home.ejs with a results object containing recipes. If called from its own page, this means it needs to filter the database before returning this. If called from somewhere else (i.e. menu bar), renders with all recipes.
   
   //ok so this one is a bit confusing. When we render it without any input, it should render /home with results as all columns for all recipes (like profile except no fancy query)
   //but when the user has selected filters, it should build a query that returns to the HTML a result object with only what the user was looking for.
   //so in the code, I think we need to be able to decide which of these things to do.
 
   //Need from HTML:
-  //HTML needs to send values for cuisine_type and all of the columns under "filters"
-  //the items sent can be empty, (in fact most of the time there will be some empty) but they should still be sent.
-  //for example, in the login POST, it sends an email, but if the user provided no email, it would be empty. This is ok.
-
-  //To be explicit, I need 
-  //cuisine_type, and all the booleans for: vegan, vegetarian, keto, paleo, grain_free, gluten_free, contains_dairy, contains_eggs, contains_nuts, contains_soy, contains_wheat, contains_beef, contains_pork, contains_fish, 
-  //and the booleans (any amount of them can be true) for under_30_minutes, m30_minutes_1_hour, h1_hour_2_hours, h2_hours_3_hours, h3_hours_or_more
-  //and the booleans (any amount of them can be true) for s1_star, s2_stars, s3_stars, s4_stars, s5_stars
-    //Note: This time, sending one star_rating instead of the 5 different star booleans will not suffice, because the user should be able to filter for all recipes that are 3 or 4 stars, for example, and sending one value cannot convey this
-
+  Nothing
 
   //Return to HTML:
   //Will return a JSON object called results (for use on the home.ejs page)
@@ -267,12 +268,22 @@ app.get("/profile", (req, res) => {
     //if you don't need to display all of the information for each recipe, you can simply use whatever you need.
     //if you wanted to, you could try to find a way to display a bunch of recipes as cards, and then when you click on one, it gives you a popup which shows all of the information. I don't know how difficult this would be.
 
+<->
+On second thought, it probably makes sense to split this into two different calls, 
+even if it has to use two more or less identical HTML pages. Therefore I suggest a 
+home.ejs which loads all of the recipes, and something like a filter.ejs, 
+which can look basically identical, but on which we will load the filtered recipes. 
+We will then have a GET /home, which will require a very easy query, and 
+a GET /filter, which will be more complicated.
+<->
+
+    */
+//DONE - NOT TESTED
 app.get("/home", (req, res) => {
-  //only the easy case implemented right now. This API call will be MUCH longer when done.y
   query = 'SELECT * FROM recipes;'; //getting all recipes and all their info
-  db.one(query, [req.session.user.username]) 
+  db.any(query, [req.session.user.user_id]) //note: MUST be db.any to return multipe query rows /recipes!
     .then(queryResult => {
-     // Send some parameters
+      //Render home page using a JSON object called results that contains all recipes for the webpage to use however it wants to.
      res.render("pages/home", {
       results: queryResult, //you will access in the EJS/HTML by calling results, not queryResult
     });
@@ -288,7 +299,75 @@ app.get("/home", (req, res) => {
 }); 
 
 
-//POST favorite - will be called by some kind of form that you use to favorite a recipe. It does not need to be its own page. Updates the database favorites table.
+/*GET /filter
+  //HTML needs to send values for cuisine_type and all of the columns under "filters"
+  //the items sent can be empty, (in fact most of the time there will be some empty) but they should still be sent.
+  //for example, in the login POST, it sends an email, but if the user provided no email, it would be empty. This is ok.
+
+  //To be explicit, I need 
+  //cuisine_type, and all the booleans for: vegan, vegetarian, keto, paleo, grain_free, gluten_free, contains_dairy, contains_eggs, contains_nuts, contains_soy, contains_wheat, contains_beef, contains_pork, contains_fish, 
+  //and the booleans (any amount of them can be true) for under_30_minutes, m30_minutes_1_hour, h1_hour_2_hours, h2_hours_3_hours, h3_hours_or_more
+  //and the booleans (any amount of them can be true) for s1_star, s2_stars, s3_stars, s4_stars, s5_stars
+    //Note: This time, sending one star_rating instead of the 5 different star booleans will not suffice, because the user should be able to filter for all recipes that are 3 or 4 stars, for example, and sending one value cannot convey this
+
+  Return to HTML:
+  Same as GET home, except will have fewer recipes
+    */
+    //IN PROGRESS
+app.get("/filter", (req, res) => {
+
+  var i = 0; //we use this because the first filter added to the query shouldn't have AND, but the rest should
+  query = 'SELECT * FROM recipes WHERE (';
+  //here we will start building the query based off of the filters the user has sent.
+  //this will basically be a lot of if statements that concatenate the query string, for example:
+
+  //recipe_name
+  if((req.body.recipe_name != '') && (req.body.recipe_name != null)){ //NOTE: the user passing a recipe_name is kind of an edge case
+    if(i == 0){
+      query = query + ` recipe_name = ${req.body.recipe_name}`;
+    }
+    else {
+      query = query + ` AND recipe_name = ${req.body.recipe_name}`;
+    }
+    i++;
+  }
+  
+  //cuisine_type
+  if((req.body.cuisine_type != null) && (req.body.cuisine_type != "")){ //Not null and not empty
+    if(i == 0){
+      query = query + ` cuisine_type = ${req.body.cuisine_type}`;
+    }
+    else {
+      query = query + ` AND cuisine_type = ${req.body.cuisine_type}`;
+    }
+    i++;
+  }
+
+
+
+  //after all the filters have been checked, finish the query
+  query = query + ');'
+
+  db.any(query, [req.session.user.user_id]) //note: MUST be db.any to return multipe query rows /recipes!
+    .then(queryResult => {
+
+      //Render HOME or different page?
+    res.render("pages/home", {
+      results: queryResult, //you will access in the EJS/HTML by calling results, not queryResult
+    });
+    })
+    .catch(err => {
+    // Handle errors, send no results and an error message to HTML
+      console.log(err);
+      res.render("pages/home", {
+        results: [],
+        message: err,
+      });
+    });
+}); 
+
+
+/*POST favorite - will be called by some kind of form that you use to favorite a recipe. It does not need to be its own page. Updates the database favorites table.
   //Need from HTML: only one thing, recipe_name
 
   //Return to HTML: Nothing
@@ -296,6 +375,22 @@ app.get("/home", (req, res) => {
   //Explanation: POST favorite will take req.body.recipe_name and use this along with req.session.user.user_id to add an entry to the favorites table. 
   //the sent recipe will then be included in the JSON object returned by GET profile next time its called.
   //redirects to home.
+  */
+  //WORKING - But handles edge cases and errors badly
+  app.post("/favorite", (req, res) => {
+  query = 'INSERT INTO favorites (user_id, recipe_id) VALUES ($1, (SELECT recipe_id FROM recipes WHERE recipe_name = $2));'; //insert a new row in favorites which is the user's id and the recipe_id of the recipe the user typed
+  db.any(query, [req.session.user.user_id, req.body.recipe_name])
+    .then(queryResult => {
+      //don't need to do anything
+    })
+    .catch(err => {
+    // Handle errors, send no results and an error message to HTML
+      console.log(err);
+      res.render("pages/home", {
+        message: err,
+      });
+    });
+}); 
 
 
 // Authentication Required
